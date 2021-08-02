eb623a66bdd86e8cb0e45214432857e0a2e12842
Maartje Eyskens <maartje@eyskens.me>
2021-08-02 11:56:41 +0200

    Pass oneclick options via json
    Signed-off-by: Maartje Eyskens <maartje@eyskens.me>

--- pkg/api/playgen.go
+++ pkg/api/playgen.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"regexp"
@@ -15,89 +17,115 @@ import (
 
 var alphaNum = regexp.MustCompile("[^a-zA-Z0-9]+")
 
-type Mode string
+type options struct {
+	URL        string `json:"url"`
+	ID         string `json:"id"`
+	Mode       mode   `json:"mode"`
+	Pages      int    `json:"pages"`
+	OneClick   bool   `json:"oneClick"`
+	FilterNoPP bool   `json:"filterNoPP"`
+}
+
+func newOptions() options {
+	return options{
+		URL:        "",
+		ID:         "",
+		Mode:       modeRecent,
+		Pages:      1,
+		OneClick:   false,
+		FilterNoPP: false,
+	}
+}
+
+type mode string
 
 const (
-	ModeRecent Mode = "recent"
-	ModeTop    Mode = "top"
+	modeRecent mode = "recent"
+	modeTop    mode = "top"
 )
 
 func init() {
 	registers = append(registers, func(e *echo.Echo, h *HTTPHandler) {
 		e.GET("/playlist/", h.GeneratePlaylist)
-		e.GET("/playlist/:id/:pages/:mode/:filename", h.GeneratePlaylistWithID)
+		e.GET("/playlist/:data/:filename", h.GeneratePlaylistWithID)
 	})
 }
 
 func (h *HTTPHandler) GeneratePlaylistWithID(c echo.Context) error {
-	pid := c.Param("id")
-
-	if pid == "" {
-		return c.JSON(http.StatusBadRequest, echo.Map{"error": "player id not specified"})
+	data, err := base64.StdEncoding.DecodeString(c.Param("data"))
+	if err != nil || len(data) == 0 {
+		return c.JSON(http.StatusBadRequest, echo.Map{"error": "invalid data payload"})
 	}
 
-	mode := ModeRecent
-	if c.Param("mode") == "top" {
-		mode = ModeTop
+	opts := newOptions()
+	err = json.Unmarshal(data, &opts)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, echo.Map{"error": "invalid data payload"})
 	}
 
-	amount := 1
-	if i, err := strconv.ParseInt(c.Param("pages"), 10, 64); err == nil && i > 0 {
-		amount = int(i)
-	}
-
-	return h.handlePlaygen(c, pid, amount, mode)
+	return h.handlePlaygen(c, opts)
 }
 
 func (h *HTTPHandler) GeneratePlaylist(c echo.Context) error {
-	url := c.QueryParam("url")
-	if url == "" {
+	opts := newOptions()
+
+	opts.URL = c.QueryParam("url")
+	if opts.URL == "" {
 		return c.JSON(http.StatusBadRequest, echo.Map{"error": "URL not specified"})
 	}
 
-	id, err := scoresaber.ExtractPlayerIDFromURL(url)
+	id, err := scoresaber.ExtractPlayerIDFromURL(opts.URL)
 	if err != nil {
 		return c.JSON(http.StatusBadRequest, echo.Map{"error": err.Error()})
 	}
+	opts.ID = id
 
-	amount := 1
 	if i, err := strconv.ParseInt(c.QueryParam("pages"), 10, 64); err == nil && i > 0 {
-		amount = int(i)
+		opts.Pages = int(i)
 	}
 
-	mode := ModeRecent
 	if c.QueryParam("mode") == "top" {
-		mode = ModeTop
+		opts.Mode = modeTop
 	}
 
 	if c.QueryParam("oneclick") == "true" {
+		opts.OneClick = true
+
 		profile, err := scoresaber.GetProfile(id)
 		if err != nil {
 			return c.JSON(http.StatusBadRequest, echo.Map{"error": err.Error()})
 		}
 
+		data, err := json.Marshal(opts)
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, echo.Map{"error": err.Error()})
+		}
+
 		// bsplaylist has no support for GET parameters
-		return c.Redirect(http.StatusTemporaryRedirect, fmt.Sprintf("bsplaylist://playlist/https://recentbeat.com/playlist/%s/%d/%s/BEAT_%s_%s_%d.bplist\n", id, amount, mode, alphaNum.ReplaceAllString(profile.PlayerInfo.PlayerName, ""), mode, time.Now().Unix()))
+		return c.Redirect(http.StatusTemporaryRedirect, fmt.Sprintf("bsplaylist://playlist/https://recentbeat.com/playlist/%s/BEAT_%s_%s_%d.bplist\n", base64.StdEncoding.EncodeToString(data), alphaNum.ReplaceAllString(profile.PlayerInfo.PlayerName, ""), opts.Mode, time.Now().Unix()))
 	}
 
-	return h.handlePlaygen(c, id, amount, mode)
+	return h.handlePlaygen(c, opts)
 }
 
-func (h *HTTPHandler) handlePlaygen(c echo.Context, id string, amount int, mode Mode) error {
+func (h *HTTPHandler) handlePlaygen(c echo.Context, opts options) error {
+	if opts.ID == "" {
+		return c.JSON(http.StatusBadRequest, echo.Map{"error": "ID not specified"})
+	}
 
-	if amount > 10 {
-		amount = 10 // sorry i am not blowing my rate limit
+	if opts.Pages > 10 {
+		opts.Pages = 10 // sorry i am not blowing my rate limit
 	}
 
 	var scores []scoresaber.Score
-	for i := 0; i < amount; i++ {
+	for i := 0; i < opts.Pages; i++ {
 		var s []scoresaber.Score
 		var err error
-		if mode == ModeRecent {
-			s, err = scoresaber.GetRecentScores(id, i+1)
+		if opts.Mode == modeRecent {
+			s, err = scoresaber.GetRecentScores(opts.ID, i+1)
 
-		} else if mode == ModeTop {
-			s, err = scoresaber.GetTopScores(id, i+1)
+		} else if opts.Mode == modeTop {
+			s, err = scoresaber.GetTopScores(opts.ID, i+1)
 		}
 		if err != nil {
 			return c.JSON(http.StatusBadRequest, echo.Map{"error": err.Error()})
@@ -110,13 +138,13 @@ func (h *HTTPHandler) handlePlaygen(c echo.Context, id string, amount int, mode 
 		scores = append(scores, s...)
 	}
 
-	profile, err := scoresaber.GetProfile(id)
+	profile, err := scoresaber.GetProfile(opts.ID)
 	if err != nil {
 		return c.JSON(http.StatusBadRequest, echo.Map{"error": err.Error()})
 	}
 
 	pl := bplist.NewPlaylist()
-	pl.PlaylistTitle = fmt.Sprintf("BEAT %s %s", mode, profile.PlayerInfo.PlayerName)
+	pl.PlaylistTitle = fmt.Sprintf("BEAT %s %s", opts.Mode, profile.PlayerInfo.PlayerName)
 	pl.PlaylistAuthor = "Recent Beater"
 
 	for _, score := range scores {
