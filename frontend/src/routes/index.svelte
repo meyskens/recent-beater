f12f1e4cfcfeb5387871210e054d2619a7936c9a
Maartje Eyskens <maartje@eyskens.me>
2021-08-02 12:12:24 +0200

    Add filterNoPP to GUI
    Signed-off-by: Maartje Eyskens <maartje@eyskens.me>

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -8,6 +8,7 @@
 	let url = "";
 	let pages = 1;
 	let mode = "recent"
+	let filterNoPP = false
 
 	async function checkURL() {
 		const res = await fetch(`https://recentbeat.com/check/?url=${encodeURIComponent(url)}`);
@@ -33,7 +34,7 @@
 			return
 		}
 
-		window.location = `/playlist/?url=${encodeURIComponent(url)}&mode=${mode}&pages=${pages}${oneclick ? "&oneclick=true" : ""}`;
+		window.location = `/playlist/?url=${encodeURIComponent(url)}&mode=${mode}&pages=${pages}${filterNoPP ? "&filterNoPP=true" : ""}${oneclick ? "&oneclick=true" : ""}`;
 		loading = false;
 	}
 
@@ -54,24 +55,24 @@
 
 <section>
 	<div class="row justify-content-center my-4">
-		<div class="col-6 about text-center">
+		<div class="col-6 col-sm-12 about text-center">
 			Recent Beater will give you a a Beat Saber playlist of the recent plays of a given ScoreSaber profile.
 		</div>
 	</div>
 	<div class="row justify-content-center my-4">
-		<div class="col-6">
+		<div class="col-12 col-md-6">
 			<input class="form-control form-control-lg" type="text" placeholder="https://scoresaber.com/u/765611979825216..." aria-label=".form-control-lg" bind:value={url}>
 		</div>
 	</div>
 	<div class="row justify-content-center my-4">
-		<div class="col-2">
+		<div class="col-6 col-md-2">
 			<label for="type">Type</label>
 			<select class="form-select" id="type" aria-label="select type" bind:value={mode}>
 				<option value="recent" selected>Recent Scores</option>
 				<option value="top">Top Scores</option>
 			</select>
 		</div>
-		<div class="col-1">
+		<div class="col-4 col-md-1">
 			<label for="type">Pages</label>
 			<select class="form-select" aria-label="select type" bind:value={pages} >
 				<option value="1" selected>1</option>
@@ -79,6 +80,13 @@
 					<option value={row+2}>{row+2}</option>
 				{/each}
 			</select>
+		</div>
+		<div class="col-2 col-md-1">
+			<label>&nbsp;</label>
+			<div class="form-check form-switch">
+				<input class="form-check-input" type="checkbox" id="filterNoPP" bind:checked={filterNoPP}>
+				<label class="form-check-label" for="filterNoPP">Filter songs with no PP</label>
+			</div>
 		</div>
 	</div>
 
